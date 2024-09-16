475316c2048519ea02e21446afa483ae69809ecc
Marcin Kurek <marcin.kurek@schibsted.com>
2024-09-16 17:51:56 +0200

    Match a literal character

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,15 +9,11 @@ fun main(args: Array<String>) {
     val pattern = args[1]
     val inputLine = System.`in`.bufferedReader().readText()
 
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.err.println("Logs from your program will appear here!")
-
-    // Uncomment this block to pass the first stage
-    // if (matchPattern(inputLine, pattern)) {
-    //     exitProcess(0)
-    // } else {
-    //     exitProcess(1)
-    // }
+     if (matchPattern(inputLine, pattern)) {
+         exitProcess(0)
+     } else {
+         exitProcess(1)
+     }
 }
 
 fun matchPattern(inputLine: String, pattern: String): Boolean {
